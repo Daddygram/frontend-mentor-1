e0f4541488028a4d3859d313ee4c2f51b7d591cf
Joseph Moninyo <tsotne.s.2004@gmail.com>
2023-12-05 16:14:17 +0400

    first-commit

---
+++ script.js
@@ -0,0 +1,16 @@
+document.querySelector('.btn1').addEventListener('click', () => {
+    document.querySelector('.popup1').classList.toggle('open')
+    document.querySelector('.btn1').classList.toggle('open')
+})
+document.querySelector('.btn2').addEventListener('click', () => {
+    document.querySelector('.popup2').classList.toggle('open')
+    document.querySelector('.btn2').classList.toggle('open')
+})
+document.querySelector('.btn3').addEventListener('click', () => {
+    document.querySelector('.popup3').classList.toggle('open')
+    document.querySelector('.btn3').classList.toggle('open')
+})
+document.querySelector('.btn4').addEventListener('click', () => {
+    document.querySelector('.popup4').classList.toggle('open')
+    document.querySelector('.btn4').classList.toggle('open')
+})
